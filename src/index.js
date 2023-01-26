64ae7d6ad7b3b182b1b6aeea142c07636fe48c3e
Pratyush Gupta <pg0623733@gmail.com>
2023-01-27 01:36:23 +0530

    Setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = () =>{
     const app = express();
     app.use(bodyParser.json());
@@ -10,6 +12,14 @@ const setupAndStartServer = () =>{
 
     app.listen(3004,() => {
         console.log(`Server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'airlinebooking338@gmail.com',
+            'pg0623733@gmail.com',
+            'This is a testing email 1',
+            'Hey,how are you!!'
+
+        )
     })
 }
 
