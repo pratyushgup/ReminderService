1d50f05f4d943cf18c7f9edf809686363fad7ebe
Pratyush Gupta <pg0623733@gmail.com>
2023-06-27 12:47:16 +0530

    Added notification Ticket model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const {PORT} = require('./config/serverConfig');
 
 const {sendBasicEmail} = require('./services/email-service');
 
+const cron = require('node-cron');
+
 const setupAndStartServer = ()=>{
     const app  = express();
     app.use(bodyParser.json());
@@ -12,12 +14,16 @@ const setupAndStartServer = ()=>{
     app.listen(PORT,async ()=>{
         console.log(`Server started at ${PORT}`);
 
-        await sendBasicEmail(
-            'airlinemanagement56@gmail.com',
-            'pg0623733@gmail.com',
-            'Third email',
-            'Our company is ready and we are still checking that everything is fine'
-        )
+        // await sendBasicEmail(
+        //     'airlinemanagement56@gmail.com',
+        //     'pg0623733@gmail.com',
+        //     'Third email',
+        //     'Our company is ready and we are still checking that everything is fine'
+        // )
+
+        cron.schedule('* * * * *', () =>  {
+            console.log('will execute every minute until stopped');
+          });
     })
 }
 
